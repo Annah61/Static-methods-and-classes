2288e912a94a24755c5ab3b040c15f36919c8e56
Annah61 <annahmoore.morerwa@gmail.com>
2023-09-18 22:41:57 +0200

    Add project files.

---
+++ Static methods and classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_methods_and_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+
+            sayHi.sayHello("Annah");
+            
+
+
+            //freeze
+            Console.ReadLine();
+        }
+    }
+}
